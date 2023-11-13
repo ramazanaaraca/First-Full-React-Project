9ccc89b01a31ee9a43ecd8bb805cae7eda935dd1
ramazanaaraca <karacaramazaan0@gmail.com>
2023-11-13 15:41:37 +0300

    Update : Forgot Password

--- src/pages/Login/ForgotPass.js
+++ src/pages/Login/ForgotPass.js
@@ -3,9 +3,17 @@ import LoginWrapper from '../../Components/LoginWrapper'
 import Input from '../../Components/Input'
 import Breadcumb from '../../Components/Breadcumb'
 import Button from '../../Components/Button'
+import { Form, Formik } from 'formik'
+import { useAuth } from '../../Context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 
 const ForgotPass = () => {
+
+  const {setMail , mail} = useAuth()
+
+  const navigate = useNavigate()
+
   return (
     <>
         <Breadcumb
@@ -14,21 +22,42 @@ const ForgotPass = () => {
           nonParagraf={true}
           />
         <div className='grid lg:grid-cols-1  gap-10 '>
-            <form action="" className='flex items-center justify-center'>
-                <LoginWrapper className={'md:w-[50%] w-[100%]'} >
-                        <p className=' text-base font-normal mb-5'>Forgot your password ? Enter your email address below. In a few minutes, we will send you an e-mail allowing you to set a new password.</p>
-                        <div className='flex flex-col gap-5'>
-                            <Input
-                            type='e-mail'
-                            LabelName='E-Mail Address'
-                            />
-                        </div>
-                        <div className='flex flex-col items-start mt-5'>
-                        <Button>Change the password</Button>
-                        </div>
-                    
-                </LoginWrapper>      
-            </form>
+            <Formik
+            initialValues={
+              {
+                mail : ''
+              }
+            }
+            onSubmit={ values => {
+              setMail(values)
+              
+              setTimeout(() => {
+                navigate('/Login')
+              }, 2000 );
+            }}
+
+            >
+              {({values}) => (
+                <Form className='flex items-center justify-center'>
+                  <LoginWrapper className={'md:w-[50%] w-[100%]'} >
+                          <p className=' text-base font-normal mb-5'>Forgot your password ? Enter your email address below. In a few minutes, we will send you an e-mail allowing you to set a new password.</p>
+                          <div className='flex flex-col gap-5'>
+                              <Input
+                              name='mail'
+                              value={values.mail}  
+                              type='e-mail'
+                              LabelName='E-Mail Address'
+                              />
+                          </div>
+                          {mail && <div className="text-sm text-green-500 font-medium" >Your Submit İs Success</div>}
+                          <div className='flex flex-col items-start mt-5'>
+                          <Button type='submit'>Change the password</Button>
+                          </div>
+                  </LoginWrapper>      
+                </Form>
+                )}
+                              
+            </Formik>
         </div>
     </>
   )
